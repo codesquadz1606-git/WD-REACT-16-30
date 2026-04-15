75f1a4efbac8fd3c71c49f6d83fbe0998ae10be1
codesquadz1606-git <codesquadz1606@gmail.com>
2026-04-15 10:11:56 +0530

    First Lecture

---
+++ react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import First ,{First1} from './Class 1/First'
+
+function App() {
+  return (
+    <>
+      <First/>
+      <First1/>
+    </>
+  )
+}
+
+export default App
---
+++ react/src/Class 1/First.jsx
@@ -0,0 +1,19 @@
+function First(){
+    // JS
+    return(
+        // HTML
+        <>
+            <h1>Hello To My First Component</h1>
+        </>
+    )
+}
+
+export function First1(){
+    return(
+        <>
+            This is new Component.
+        </>
+    )
+}
+
+export default First;
